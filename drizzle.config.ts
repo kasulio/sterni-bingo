d929a15c9211601866e87a0b0baf211174b66910
Lukas Frey <hey@lukasfrey.com>
2026-04-15 14:36:58 +0200

    upgrade drizzle version

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,10 @@
 import type { Config } from "drizzle-kit";
 
 export default {
-    schema: "./src/db/schema.ts",
-    out: "./src/drizzle",
-    driver: "turso",
-    dbCredentials: {
-        url: `file:${process.env.DB_URL}`,
-    },
-    dialect: "sqlite",
+  schema: "./src/db/schema.ts",
+  out: "./src/drizzle",
+  dbCredentials: {
+    url: `file:${process.env.DB_URL}`,
+  },
+  dialect: "sqlite",
 } satisfies Config;
